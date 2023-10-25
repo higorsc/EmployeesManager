c65a73ecd9fb79f52fc171f33e673742b96f3d3a
Higor Carvalho <higorsc_2@hotmail.com>
2023-10-25 20:15:41 -0300

    MainWindow and Employee view models for interaction with views

---
+++ EmployeesManager/ViewModels/CreateEmployeeViewModel.cs
@@ -0,0 +1,137 @@
+﻿using EmployeesManager.Abstractions;
+using EmployeesManager.Commands;
+using EmployeesManager.Models.DTO;
+using EmployeesManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static EmployeesManager.Models.Enums.GenderEnum;
+using static EmployeesManager.Models.Enums.StatusEnum;
+using EmployeesManager.Services.Interfaces;
+using EmployeesManager.Helpers;
+using System.Windows;
+
+namespace EmployeesManager.ViewModels
+{
+    public class CreateEmployeeViewModel : BaseNotifyPropertyChanged
+    {
+        private readonly IEmployeesService _employeesService;
+        private readonly Mapper _mapper;
+
+        public CreateEmployeeViewModel(IEmployeesService employeesService, Mapper mapper)
+        {
+            _employeesService = employeesService;
+            _mapper = mapper;
+        }
+
+        #region PrivateProperties
+
+        private string _name { get; set; }
+
+
+
+        private string _email { get; set; }
+
+
+        private Gender _gender { get; set; }
+
+        
+
+        private EmployeeStatus _status { get; set; }
+
+        private IEnumerable<Gender> _genders { get; set; }
+        
+        #endregion Properties
+
+        #region PublicProperties
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+
+        public Gender Gender
+        {
+            get
+            {
+                return _gender;
+            }
+
+            set
+            {
+                _gender = value;
+            }
+        }
+
+        public EmployeeStatus Status
+        {
+            get
+            {
+                return _status;
+            }
+            set
+            {
+                _status = value;
+            }
+        }
+
+        public IEnumerable<EmployeeStatus> Statuses
+        {
+            get
+            {
+                var statuses = Enum
+                    .GetValues(Status.GetType())
+                    .Cast<EmployeeStatus>();
+
+                return statuses;
+            }
+            set { }
+        }
+
+        public IEnumerable<Gender> Genders
+        {
+            get
+            {
+                var genders = Enum
+                    .GetValues(Gender.GetType())
+                    .Cast<Gender>();
+
+                return genders;
+            }
+            set
+            {
+                _genders = value;
+            }
+        }
+
+        #endregion PublicProperties
+
+        #region Commands
+        public CreateEmployeeCommand CreateCommand => new CreateEmployeeCommand(_employeesService, _mapper, this);
+        #endregion Commands
+    }
+}
---
+++ EmployeesManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,107 @@
+﻿using EmployeesManager.Abstractions;
+using EmployeesManager.Commands;
+using EmployeesManager.Helpers;
+using EmployeesManager.Models;
+using EmployeesManager.Services;
+using EmployeesManager.Services.Interfaces;
+using EmployeesManager.Views;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesManager.ViewModels
+{
+    public class MainWindowViewModel : BaseNotifyPropertyChanged
+    {
+        private static IEmployeesService _employeesService;
+        private static Mapper _mapper;
+
+        public MainWindowViewModel(IEmployeesService employeesService, Mapper mapper, IDialogService dialogService)
+        {
+            _employeesService = employeesService;
+            _mapper = mapper;
+
+            App.Current.Dispatcher.Invoke(async () =>
+            {
+                await LoadEmployees();
+            });
+        }
+
+        public ObservableCollection<Employee> Employees => Models.EmployeesManager.Employees;
+
+        public ShowWindowCommand ShowWindowCommand => new ShowWindowCommand(ShowAddEmployeeWindow);
+
+        public string FileHandlerExecutionClass { get; set; }
+
+        public FileHandlerCommand FileHandlerCommand =>
+                  new FileHandlerCommand(FileHandlerExecutionClass, Employees);
+
+        private string _searchFilter;
+
+        public string SearchFilter
+        {
+            get
+            {
+                return _searchFilter;
+            }
+            set
+            {
+                SetField<string>(ref _searchFilter, value);
+
+                _searchFilter = value;
+                LoadEmployeesByName();
+            }
+        }
+
+        private async Task LoadEmployees()
+        {
+            var employees = await _employeesService.GetEmployees();
+
+            foreach (var employee in employees)
+            {
+                try
+                {
+                    Models.EmployeesManager.Employees.Add(employee);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private async Task LoadEmployeesByName()
+        {
+            var employees = await _employeesService.GetEmployeesByName(_searchFilter);
+
+            Models.EmployeesManager.Employees.Clear();
+
+            foreach (var employee in employees)
+            {
+                try
+                {
+                    Models.EmployeesManager.Employees.Add(employee);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+
+
+        }
+
+        private  async Task ShowAddEmployeeWindow(object parameter)
+        {
+            var window = new CreateEmployeeWindow(_employeesService, _mapper);
+
+            window.ShowDialog();
+        }
+    }
+}
