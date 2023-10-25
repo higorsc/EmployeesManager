0844b7e6867a1324da60134ce7fec49e2af651a4
Higor Carvalho <higorsc_2@hotmail.com>
2023-10-25 20:14:45 -0300

    Implemented Command classes

--- EmployeesManager/Commands/CreateEmployeeCommand.cs
+++ EmployeesManager/Commands/CreateEmployeeCommand.cs
@@ -1,9 +1,17 @@
 ﻿using EmployeesManager.Abstractions;
+using EmployeesManager.Helpers;
+using EmployeesManager.Models.DTO;
+using EmployeesManager.Models;
+using EmployeesManager.Services;
+using EmployeesManager.Services.Interfaces;
+using EmployeesManager.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+using static EmployeesManager.Models.Enums.GenderEnum;
 
 namespace EmployeesManager.Commands
 {
@@ -11,13 +19,47 @@ namespace EmployeesManager.Commands
     {
         private Func<object, Task> _action;
 
+        private readonly IEmployeesService _employeeService;
+        private readonly Mapper _mapper;
+        private readonly CreateEmployeeViewModel _createEmployeeViewModel;
+
         public CreateEmployeeCommand(Func<object, Task> action)
         {
             _action = action;
         }
 
+        public CreateEmployeeCommand(IEmployeesService employeeService, Mapper mapper, CreateEmployeeViewModel createEmployeeViewModel)
+        {
+            _mapper = mapper;
+            _employeeService = employeeService;
+            _createEmployeeViewModel = createEmployeeViewModel;
+        }
+
         public override async Task ExecuteAsync(object? parameter)
         {
+            try
+            {
+                var viewModel = parameter as CreateEmployeeViewModel;
+
+                var employeeDto = new EmployeeDTO()
+                {
+                    Name = _createEmployeeViewModel.Name,
+                    Gender = _createEmployeeViewModel.Genders.FirstOrDefault(a => a == _createEmployeeViewModel.Gender).ToString(),
+                    Status = _createEmployeeViewModel.Statuses.FirstOrDefault(a => a == _createEmployeeViewModel.Status).ToString(),
+                    Email = _createEmployeeViewModel.Email
+                };
+
+                var employee = new Employee();
+
+                var createdEmployee = await _employeeService.AddEmployee(employeeDto);
+
+                _mapper.CustomMapping(a => Mappings.MapEmployeesFromDTO(createdEmployee, employee));
+
+                Models.EmployeesManager.Employees.Add(employee);
+            }
+            catch (Exception ex)
+            {
+            }
             _action?.Invoke(parameter);
         }
     }
--- EmployeesManager/Commands/FileHandlerCommand.cs
+++ EmployeesManager/Commands/FileHandlerCommand.cs
@@ -1,8 +1,12 @@
 ﻿using EmployeesManager.Abstractions;
+using EmployeesManager.Models;
 using EmployeesManager.Services;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,15 +15,33 @@ namespace EmployeesManager.Commands
     public class FileHandlerCommand : BaseCommand
     {
         private Func<object, Task> _action;
+        private readonly ObservableCollection<Employee> _employees;
+        private readonly string _handlerClassName;
 
         public FileHandlerCommand(Func<object, Task> action, Type t)
         {
             _action = action;
         }
 
+        public FileHandlerCommand(string handlerClassName, ObservableCollection<Employee> employees)
+        {
+            _handlerClassName = handlerClassName;
+            _employees = employees;
+        }
+
         public override async Task ExecuteAsync(object? parameter)
         {
-            await _action?.Invoke(parameter);
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Comma Separated Values (*.csv)|*.csv";
+            saveFileDialog.DefaultExt = "Comma Separated Values (*.csv)";
+            saveFileDialog.ShowDialog();
+
+            var fileName = saveFileDialog.FileName;
+
+            var type = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(a => a.Name == parameter.ToString());
+
+            var instance = (IBaseFileHandler)Activator.CreateInstance((type));
+            instance.SaveFile<Employee>(_employees.ToList(), fileName);
         }
     }
 }
