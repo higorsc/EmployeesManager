01f33f37861ce2467543416daa6ffa2c1da7d5da
Higor Carvalho <higorsc_2@hotmail.com>
2023-10-25 20:17:12 -0300

    Added dependency injection and services registrations.

--- EmployeesManager/App.xaml.cs
+++ EmployeesManager/App.xaml.cs
@@ -1,17 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using EmployeesManager.Helpers;
+using EmployeesManager.Repositories;
+using EmployeesManager.Repositories.Interfaces;
+using EmployeesManager.Services;
+using EmployeesManager.Services.Interfaces;
+using EmployeesManager.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace EmployeesManager
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static IHost? AppHost { get; private set; }
+
+        public App()
+        {
+            AppHost = Host
+                .CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                    services.AddSingleton<MainWindowViewModel>();
+                    services.AddScoped<IEmployeesRepository, EmployeesRepository>();
+                    services.AddScoped<IEmployeesService, EmployeesService>();
+                    services.AddSingleton<Mapper>();
+                    services.AddSingleton<IDialogService, DialogService>();
+                    services.AddHttpClient();
+                })
+                .Build();
+
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost.StartAsync();
+
+            var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+            startupForm.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
