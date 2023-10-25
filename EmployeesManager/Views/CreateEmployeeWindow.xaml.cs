6b33ed07347c4041ab25a8c22b4be07478a04116
Higor Carvalho <higorsc_2@hotmail.com>
2023-10-25 20:16:30 -0300

    Change MainWindow namespace and added view for adding new employees

---
+++ EmployeesManager/Views/CreateEmployeeWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using EmployeesManager.Helpers;
+using EmployeesManager.Services.Interfaces;
+using EmployeesManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EmployeesManager.Views
+{
+    /// <summary>
+    /// Interaction logic for CreateEmployeeWindow.xaml
+    /// </summary>
+    public partial class CreateEmployeeWindow : Window
+    {
+        public CreateEmployeeWindow(IEmployeesService employeesService, Mapper mapper)
+        {
+            InitializeComponent();
+            DataContext = new CreateEmployeeViewModel(employeesService, mapper);
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
--- EmployeesManager/Views/MainWindow.xaml.cs
+++ EmployeesManager/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeesManager.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,10 @@ namespace EmployeesManager
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow(MainWindowViewModel mainWindowViewModel)
         {
             InitializeComponent();
+            DataContext = mainWindowViewModel;
         }
     }
 }
